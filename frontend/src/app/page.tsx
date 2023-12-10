d452e05044407184dc32f898dd5bbfdf242844e5
Craig Smith <craig@craigsmith.dev>
2023-12-10 00:23:28 -0600

    Prevent ios safari zooming into text fields

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -144,9 +144,12 @@ const GameComponent = () => {
   }
 
   return (
-
-    router()
-
+    <>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
+      {router()}
+    </>
   );
 }
 
