c745da99e92f0241f0064593f6200aa33b23741c
Craig Smith <craig@craigsmith.dev>
2023-12-09 23:57:08 -0600

    Player count optional

--- frontend/src/app/components/LobbyCard.tsx
+++ frontend/src/app/components/LobbyCard.tsx
@@ -4,11 +4,12 @@ import styles from './lobbyCard.module.css'
 
 import { Text, Paper } from '@mantine/core';
 
-export default function LobbyCard(props: { lobbyId: number, playerCount: number, onClick: () => void }) {
+export default function LobbyCard(props: { lobbyId: number, playerCount?: number, onClick: () => void }) {
     return (
         <Paper className={styles.lobbyButton} onClick={props.onClick} shadow="sm" withBorder p="xl">
             <Text ta="center">{props.lobbyId}</Text>
-            <Text c="dimmed" ta="center">{`${props.playerCount} players`}</Text>
+            {props.playerCount ? <Text c="dimmed" ta="center">{`${props.playerCount} players`}</Text> : undefined}
+
         </Paper >
     )
 }
