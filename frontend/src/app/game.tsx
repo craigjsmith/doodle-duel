b9201512549593727e2979980086dca1935d1431
Craig Smith <craig@craigsmith.dev>
2023-12-10 15:48:00 -0600

    Fixed bar on game UI

--- frontend/src/app/game.tsx
+++ frontend/src/app/game.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { Container, Flex, Title, Button, Text, SimpleGrid, Modal } from '@mantine/core';
+import styles from './game.module.css'
 
-import { useEffect, useState } from 'react';
+import { Button, Input, Center, Group } from '@mantine/core';
+
+import { useEffect, useState, useRef } from 'react';
 import Timer from './timer';
 import Whiteboard from './whiteboard';
 
@@ -18,26 +20,51 @@ export default function Game(props: {
     guess: () => void,
 }) {
     const [lobbyList, setLobbyList] = useState<Array<number>>();
+    const guessBox = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
-
+        window.addEventListener("resize", () => {
+            if (guessBox.current) {
+                guessBox.current.style.bottom = (window.innerHeight - window.innerHeight) + 'px';
+            }
+        });
     }, []);
 
     return (
         <>
-            {props.secretWord ? <h1>{props.secretWord}</h1> : undefined}
-            {props.revealWord ? <h1>{props.previousWord}</h1> : undefined}
             <Timer endTimestamp={props.endTimestamp ?? 0} duration={20} />
             <Whiteboard image={props.image} draw={props.draw} enable={props.isMyTurn} />
-            <br />
-            {!props.isMyTurn ?
-                <>
-                    <input type="text" onChange={(event) => { props.setWordGuess(event.target.value) }}></input>
-                    <button onClick={() => {
-                        props.guess();
-                    }}>guess</button>
-                </>
-                : undefined}
+
+            <div className={styles.fixedTextBox}>
+                {!props.isMyTurn ?
+                    <Center>
+                        <Group>
+                            <Input
+                                size="m"
+                                radius="md"
+                                type="text"
+                                placeholder="Your guess"
+                                onChange={(event) => { props.setWordGuess(event.target.value) }}
+                            />
+
+                            <Button
+                                variant="bright"
+                                size="s"
+                                radius="md"
+                                my={15}
+                                onClick={() => { props.guess() }}
+                            >
+                                Guess
+                            </Button>
+                        </Group>
+                    </Center>
+
+                    :
+                    <Center>
+                        {props.revealWord ? <h1>{props.previousWord}</h1> : (props.secretWord ? <h1>You are drawing: {props.secretWord}</h1> : undefined)}
+                    </Center>
+                }
+            </div>
         </>
     )
 }
