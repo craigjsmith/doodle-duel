0e5f90d9733a943239f0eaa62ada7c7d00b23f6f
Craig Smith <craig@craigsmith.dev>
2023-12-12 20:43:12 -0600

    Lobby screen styling

--- frontend/src/app/Lobby.tsx
+++ frontend/src/app/Lobby.tsx
@@ -33,75 +33,79 @@ export default function Lobby(props: { lobbyId: number | null, players: Player[]
     }, []);
 
     return (
-        <Container>
-            <Flex
-                mih={50}
-                gap="md"
-                justify="center"
-                align="center"
-                direction="column"
-                wrap="wrap"
-            >
+        <>
+            <div className={styles.topBar}>
                 <Title order={1} my={20}>{`Lobby ${props.lobbyId}`}</Title>
-
-                <Flex direction="column" align="center" my={50}>
-                    <Text size="md" mb={4}>Share this link to invite players</Text>
-                    <CopyButton value={URL}>
-                        {({ copied, copy }) => (
-                            <Group gap={0}>
-                                <Input
-                                    variant='filled'
-                                    radius={0}
-                                    type="text"
-                                    value={URL}
-                                    className={styles.linkInput}
-                                    onClick={() => { copy() }}
-                                    mx={0}
-                                />
-
-                                <Button onClick={() => { copy() }} radius={0} mx={0}>
-                                    {copied ? 'Copied' : 'Copy'}
-                                </Button>
-                            </Group>
-                        )}
-                    </CopyButton>
-                </Flex>
-
-                <List
-                    spacing="xs"
-                    size="sm"
-                    center
-                    icon={
-                        <ThemeIcon variant="light" size={33} radius="xl">
-                            <IconUser style={{ width: rem(16), height: rem(16) }} />
-                        </ThemeIcon>
-                    }
+            </div>
+            <Container>
+                <Flex
+                    mih={50}
+                    gap="md"
+                    justify="center"
+                    align="center"
+                    direction="column"
+                    wrap="wrap"
                 >
-                    {props.players?.map((player) => <List.Item><Text size="xl">{player.username}</Text></List.Item>)}
-                </List>
 
-                {
-                    (props.players?.length ?? 0) > 1 ?
-                        <Button
-                            variant="filled"
-                            radius="xl"
-                            my={40}
-                            onClick={() => { props.startGame() }}
-                        >
-                            Start
-                        </Button>
-                        :
-                        <Flex
-                            my={40}
-                            justify="center"
-                            align="center"
-                            direction="column">
-                            <Loader type="dots" />
-                            <Text size="md">Waiting for more players</Text>
-                        </Flex>
-                }
+                    <Flex direction="column" align="center" my={50}>
+                        <Text size="md" mb={4}>Share this link to invite players</Text>
+                        <CopyButton value={URL}>
+                            {({ copied, copy }) => (
+                                <Group gap={0}>
+                                    <Input
+                                        variant='filled'
+                                        radius={0}
+                                        type="text"
+                                        value={URL}
+                                        className={styles.linkInput}
+                                        onClick={() => { copy() }}
+                                        mx={0}
+                                    />
 
-            </Flex>
-        </Container>
+                                    <Button onClick={() => { copy() }} radius={0} mx={0}>
+                                        {copied ? 'Copied' : 'Copy'}
+                                    </Button>
+                                </Group>
+                            )}
+                        </CopyButton>
+                    </Flex>
+
+                    <List
+                        spacing="xs"
+                        size="sm"
+                        center
+                        icon={
+                            <ThemeIcon variant="light" size={33} radius="xl">
+                                <IconUser style={{ width: rem(16), height: rem(16) }} />
+                            </ThemeIcon>
+                        }
+                    >
+                        {props.players?.map((player) => <List.Item><Text size="xl">{player.username}</Text></List.Item>)}
+                    </List>
+
+                    {
+                        (props.players?.length ?? 0) > 1 ?
+                            <Button
+                                variant="filled"
+                                radius="xl"
+                                my={40}
+                                onClick={() => { props.startGame() }}
+                            >
+                                Start
+                            </Button>
+                            :
+                            <Flex
+                                my={40}
+                                justify="center"
+                                align="center"
+                                direction="column">
+                                <Loader type="dots" />
+                                <Text size="md">Waiting for more players</Text>
+                            </Flex>
+                    }
+
+                </Flex>
+            </Container>
+        </>
     )
 }
