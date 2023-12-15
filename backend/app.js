04cb97814b2dfd0f5b64f116f3eb1af08ba2a5ea
Craig Smith <craig@craigsmith.dev>
2023-12-15 00:04:20 -0600

    Emit draw only if artist is still current turn

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,8 @@ const ROUND_DURATION = 20000;
 const END_OF_ROUND_DURATION = 6000;
 const GRACE_DURTION = 1000;
 
+let currentArtistId;
+
 let bouncer = new LobbyBouncer();
 
 // key: game id, value: timeout reference
@@ -143,7 +145,9 @@ async function removeLobby(lobbyId) {
 }
 
 function onDraw(msg, id) {
-    io.to(id).volatile.emit('DRAW', msg);
+    if (currentArtistId && !JSON.parse(msg).artist.localeCompare(currentArtistId)) {
+        io.to(id).volatile.emit('DRAW', msg);
+    }
 }
 
 async function gameOver(lobbyId) {
@@ -215,6 +219,8 @@ const setNewWord = async (id) => {
     await db.setPreviousWord(id, previousWord);
 
     let artist = JSON.parse(gameState.turn);
+
+    currentArtistId = artist.socketId;
 
     // Reveal secret word only to the artist
     io.to(artist.socketId).emit('REVEAL', newWord);
