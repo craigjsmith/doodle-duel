619462a76406da8ea1faa3d5ba8e69c4ceb68545
Craig Smith <craig@craigsmith.dev>
2023-12-14 22:53:21 -0600

    Favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <ColorSchemeScript />
+        <title>Doodle Duel</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <link rel="shortcut icon" href="/images/icon.svg" />
       </head>
       <body>
         <MantineProvider
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -143,9 +143,6 @@ const PageComponent = () => {
 
   return (
     <>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-      </head>
       {screenRouter()}
     </>
   );
