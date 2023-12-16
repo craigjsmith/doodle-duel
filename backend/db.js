b5deaefb082effdab33d96bba5584aa29c82fce4
Craig Smith <craig@craigsmith.dev>
2023-12-15 23:00:25 -0600

    Store credentials are environment variables

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
+const env = require('dotenv').config()
 
 // DBMS setup
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'app',
-    password: 'E2MLAWX92Tz&tMcnfGm&',
-    database: 'db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
 connection.connect((error) => {
