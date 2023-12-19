d9974faf359cfa32f94a9fa74aa9e074f8f0bcc6
Craig Smith <craig@craigsmith.dev>
2023-12-19 11:57:37 -0600

    Force websocket transport (no long poll)

--- frontend/src/app/socket.ts
+++ frontend/src/app/socket.ts
@@ -4,5 +4,6 @@ import { io } from "socket.io-client";
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
 
 export const socket = io("https://droplet.craigsmith.dev", {
-    autoConnect: false
-  });
+	autoConnect: false,
+	transports: ["websocket"],
+});
