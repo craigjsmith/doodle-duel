17237d941a743b03695621a792097598574bdeba
Craig Smith <craig@craigsmith.dev>
2023-12-26 21:21:55 -0500

    Settings for higher latency

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -78,7 +78,7 @@ const PageComponent = () => {
   };
 
   const emitDrawing = (img: Uint8ClampedArray | undefined) => {
-    socket.volatile.emit('NEWDRAW', { img: img, artist: socket.id }, lobby);
+    socket.emit('NEWDRAW', { img: img, artist: socket.id }, lobby);
   };
 
   const startLobby = async () => {
