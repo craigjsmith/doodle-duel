b4e4e314bb0bf89a515625b3f26aa3d49011dc4a
Craig Smith <craig@craigsmith.dev>
2025-06-09 22:21:02 -0500

    gcp use us-central

--- frontend/src/app/socket.ts
+++ frontend/src/app/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://doodle-duel-896989562989.europe-west1.run.app", {
+export const socket = io("https://doodle-duel-896989562989.us-central1.run.app", {
 	autoConnect: false,
 	transports: ["websocket"],
 });
